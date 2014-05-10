2e0044f4c05805241e0ac46efda471e9be74f7dc
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2014-05-10 14:33:07 +0200

    changes

--- WindowManager.java
+++ WindowManager.java
@@ -9,6 +9,8 @@ import java.util.List;
  */
 public class WindowManager extends Widget{ 
 	Window win; 
+        CloseButton close ;
+        MinimiseButton min ;
 	
 	public WindowManager( ){  
 	}
@@ -18,7 +20,15 @@ public class WindowManager extends Widget{
 		positionY = win.getPositionY() - Constants.title_bar_height;
 		
 		width = win.width;
-		height = Constants.title_bar_height;  
+		height = Constants.title_bar_height;
+                
+//                min.setHeight(Constants.minimise_button_height);
+  //              min.setWidth(Constants.minimise_button_width);
+    //            min.setPositionX(positionX + 2 ) ;
+      //          min.setPositionY(positionY + 2 ) ;
+                 
+               close = new CloseButton(Constants.close_button_width ,Constants.close_button_height , positionX + Constants.minimise_button_width +  2  , positionY + Constants.minimise_button_width + 2 );
+
 	}
 	
 	//draw title bar
@@ -37,6 +47,8 @@ public class WindowManager extends Widget{
 		// fill bg color
 		ges.setColor(new Color(138,226,237)); 
 		ges.fillRect(positionX, positionY, positionX + width, positionY + height);
+                close.drawButton(ges);
+                
 	}	
 	
 	// handle mouse drag
