fe6a833532fa0bacab787982af8e057b03528a79
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2014-05-10 19:37:58 +0200

    UICallingOnMainThread

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 
+import javax.swing.SwingUtilities;
+
+
 public class Main {
     public static final int width = 1024;
     public static final int height = 800;
 
     public static void main(String[] args) {
         // desktop
+        
+        SwingUtilities.invokeLater(new Runnable() {
+    @Override
+    public void run() {
+        
         Desktop desktop = new Desktop();
 
         // example create window inside the desktop
@@ -17,6 +25,10 @@ public class Main {
         //desktop1.CreateWindow("main window", 640, 480);
 
         //desktop1.CreateWindow("second window", new Vector2(400, 200), 100, 200);
+        
+    }
+});
+        
 
     }
 }
