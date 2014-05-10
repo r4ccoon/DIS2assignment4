20dca26e4405a1934c8f317d25d7b2149a36badc
ridho <rpl.oye@gmail.com>
2014-05-10 17:53:47 +0200

    indentation

--- Widget.java
+++ Widget.java
@@ -1,47 +1,48 @@
 
-public class Widget { 
-	protected int width;
-	protected int height;
-	protected int positionX;
-	protected int positionY;
-        protected String tittle ;
+public class Widget {
+    protected int width;
+    protected int height;
+    protected int positionX;
+    protected int positionY;
+    protected String tittle ;
 
-        public String getTittle(){
-            return tittle ;
-        }
-        
-        public void setTittle(String tittle){
-            this.tittle = tittle ;
-        }
-	public int getWidth() {
-		return width;
-	}
+    public String getTittle(){
+        return tittle ;
+    }
+    
+    public void setTittle(String tittle){
+        this.tittle = tittle ;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public void setWidth(int width) {
+        this.width = width;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public int getPositionX() {
-		return positionX;
-	}
+    public int getPositionX() {
+        return positionX;
+    }
 
-	public void setPositionX(int positionX) {
-		this.positionX = positionX;
-	}
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
 
-	public int getPositionY() {
-		return positionY;
-	}
+    public int getPositionY() {
+        return positionY;
+    }
 
-	public void setPositionY(int positionY) {
-		this.positionY = positionY;
-	}
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
 }
