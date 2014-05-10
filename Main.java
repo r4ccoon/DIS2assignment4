4a9035096e6e651fe6f654e7e2e99b7f47290afc
ridho <rpl.oye@gmail.com>
2014-05-10 20:36:41 +0200

    fixing the indenttin

--- Main.java
+++ Main.java
@@ -8,27 +8,27 @@ public class Main {
 
     public static void main(String[] args) {
         // desktop
-        
+
         SwingUtilities.invokeLater(new Runnable() {
-    @Override
-    public void run() {
-        
-        Desktop desktop = new Desktop();
+            @Override
+            public void run() {
 
-        // example create window inside the desktop
-        desktop.CreateWindow("main window", 640, 480);
+                Desktop desktop = new Desktop();
 
-        // example create window inside the desktop
-        desktop.CreateWindow("second window", new Vector2(400, 200), 100, 200);
+                // example create window inside the desktop
+                desktop.CreateWindow("main window", 640, 480);
 
-        //Desktop desktop1 = new Desktop();
-        //desktop1.CreateWindow("main window", 640, 480);
+                // example create window inside the desktop
+                desktop.CreateWindow("second window", new Vector2(400, 200), 100, 200);
 
-        //desktop1.CreateWindow("second window", new Vector2(400, 200), 100, 200);
-        
-    }
-});
-        
+                //Desktop desktop1 = new Desktop();
+                //desktop1.CreateWindow("main window", 640, 480);
+
+                //desktop1.CreateWindow("second window", new Vector2(400, 200), 100, 200);
+
+            }
+        });
+
 
     }
 }
